d1809ae001de14e2ba6df686d1c22ea5ccee741c
FlackCode <fl4ckc0de@gmail.com>
2025-01-10 21:41:30 +0200

    Finished game, time to cleaup code

--- public/js/main.js
+++ public/js/main.js
@@ -42,18 +42,26 @@ const apple = new Apple();
 function input(event) {
     switch (event.code) {
         case "ArrowUp":
+            snake.velocity.x = 0;
             snake.velocity.y = -1;
             break;
         case "ArrowDown":
+            snake.velocity.x = 0;
             snake.velocity.y = +1;
+            break;
         case "ArrowLeft":
             snake.velocity.x = -1;
+            snake.velocity.y = 0;
+            break;
         case "ArrowRight":
             snake.velocity.x = +1;
+            snake.velocity.y = 0;
+            break;
     }
 }
 
 function gameLoop() {
+    window.addEventListener("keydown", event => input(event));
     snake.position.x += snake.velocity.x;
     snake.position.y += snake.velocity.y;
 
@@ -78,10 +86,17 @@ function gameLoop() {
 
     context.fillStyle = "lime";
     for (let i = 0; i < snake.trail.length; i++) {
-        context.fillRect(snake.trail[i].position.x * gridSize, snake.trail[i].position.y * gridSize, gridSize-2, gridSize-2);
-        if (snake.trail[i].position.x == snake.position.x && snake.trail[i].position.y == snake.position.y) {
+        context.fillRect(snake.trail[i].x * gridSize, snake.trail[i].y * gridSize, gridSize-2, gridSize-2);
+        if (snake.trail[i].x == snake.position.x && snake.trail[i].y == snake.position.y) {
             snake.tail = 5;
         }
+    }
+    snake.trail.push({
+        x: snake.position.x,
+        y: snake.position.y
+    });
+    while(snake.trail.length > snake.tail) {
+        snake.trail.shift();
     }
 
     if (apple.position.x == snake.position.x && apple.position.y == snake.position.y) {
