e5cb9f4277b750953258a9b1742101463349453b
FlackCode <fl4ckc0de@gmail.com>
2025-01-10 21:27:54 +0200

    Added wrapping and an apple

--- public/js/main.js
+++ public/js/main.js
@@ -30,6 +30,7 @@ const velocity = new Velocity();
 const position = new Position();
 const gridSize = 20;
 const tileCount = 20;
+const apple = new Apple();
 
 function input(event) {
     switch (event.code) {
@@ -44,3 +45,26 @@ function input(event) {
             velocity.x = +1;
     }
 }
+
+function gameLoop() {
+    position.x += velocity.x;
+    position.y += velocity.y;
+
+    if (position.x < 0) {
+        position.x = tileCount-1;
+    }
+    if (position.x > tileCount-1) {
+        position.x = 0;
+    }
+    if (position.y < 0) {
+        position.y = tileCount-1;
+    }
+    if (position.y > tileCount-1) {
+        position.y = 0;
+    }
+
+    context.fillStyle = "red";
+    context.fillRect(apple.position.x * gridSize, apple.position.y * gridSize, gridSize-2, gridSize-2);
+}
+
+setInterval(gameLoop, 1000/15);
